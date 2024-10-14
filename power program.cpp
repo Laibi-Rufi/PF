159bf1d5d1fc0a4d528c2206574856a2694800f2
Laibi-Rufi <saimroofi@gmail.com>
2024-10-14 10:26:58 +0500

    Table of 2

---
+++ power program.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+	
+	int base;
+	int exponent;
+	
+	cout<<"Enter the base : ";
+	cin>>base;
+	
+	cout<<"Enter the exponent : ";
+	cin>>exponent;
+	
+	 
+	 cout<<pow(base,exponent);
+	 
+	 return 0;
+}
+
